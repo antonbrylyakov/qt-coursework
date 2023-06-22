ba6fb0db635cb25332126cda155702555e637d85
Антон Брыляков <yoll@rambler.ru>
2023-06-22 07:26:52 -0700

    Расписание рейсов по местному времени

--- flightdataaccessor.h
+++ flightdataaccessor.h
@@ -24,16 +24,16 @@ public:
     const FlightDirection getFlightDirection() const;
     void setFlightDirection(const FlightDirection direction);
 protected:
-    inline static const QString QUERY_TEXT_DEPARTURE = "SELECT flight_no, scheduled_departure, ad.airport_name->>'ru' as airportName from bookings.flights f "
+    inline static const QString QUERY_TEXT_DEPARTURE = "SELECT flight_no, scheduled_departure at time zone ad.timezone, ad.airport_name->>'ru' as airportName from bookings.flights f "
                                                        "JOIN bookings.airports_data ad ON ad.airport_code = f.arrival_airport "
                                                        "where f.departure_airport = :airportCode "
-                                                       "AND f.scheduled_departure >= :date AND f.scheduled_departure < (:date::timestamptz + interval '1 day') "
+                                                       "AND f.scheduled_departure >= :date::timestamp at time zone ad.timezone AND f.scheduled_departure < (:date::timestamp at time zone ad.timezone + interval '1 day') "
                                                        "ORDER BY scheduled_departure";
 
-    inline static const QString QUERY_TEXT_ARRIVAL = "SELECT flight_no, scheduled_arrival, ad.airport_name->>'ru' as airportName from bookings.flights f "
+    inline static const QString QUERY_TEXT_ARRIVAL = "SELECT flight_no, scheduled_arrival at time zone ad.timezone, ad.airport_name->>'ru' as airportName from bookings.flights f "
                                                      "JOIN bookings.airports_data ad ON ad.airport_code = f.departure_airport "
                                                      "WHERE f.arrival_airport = :airportCode "
-                                                     "AND f.scheduled_arrival >= :date AND f.scheduled_arrival < (:date::timestamptz + interval '1 day') "
+                                                     "AND f.scheduled_arrival >= :date::timestamp at time zone ad.timezone AND f.scheduled_arrival < (:date::timestamp at time zone ad.timezone + interval '1 day') "
                                                      "ORDER BY scheduled_arrival";
     QString m_airPortCode;
     QDate m_date;
